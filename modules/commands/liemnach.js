21e60a4b43e49c9546a39ba146bf35c7e3318ad3
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-06 22:21:11 +0700

    Create liemnach.js

---
+++ modules/commands/liemnach.js
@@ -0,0 +1,73 @@
+module.exports.config = {
+    name: "liemnach",
+    version: "2.0.0",
+    hasPermssion: 0,
+    credits: "Pro",
+    description: "",
+    commandCategory: "Game",
+    usages: "[tag]",
+    cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
+    }
+}
+
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    if (!fs.existsSync(dirMaterial + "canvas")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "liemnach.png")) request("https://i.imgur.com/dgg7t4Q.jpg").pipe(fs.createWriteStream(dirMaterial + "liemnach.png"));
+}
+
+async function makeImage({ one, two }) {    
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const path = require("path");
+    const jimp = require("jimp");
+    const __root = path.resolve(__dirname, "cache", "canvas");
+
+    let liemnach_image = await jimp.read(__root + "/liemnach.png");
+    let pathImg = __root + `/liemnach_${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${two}.png`;
+    let avatarTwo = __root + `/avt_${one}.png`;
+    
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+    
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    liemnach_image.composite(circleOne.resize(220, 220), 316, 204).composite(circleTwo.resize(170, 170), 46, 584);
+    
+    let raw = await liemnach_image.getBufferAsync("image/png");
+    
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+    
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+
+module.exports.run = async function ({ event, api, args }) {
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
+    const mention = Object.keys(event.mentions);
+    if (!mention[0]) return api.sendMessage("Vui lòng tag 1 người.", threadID, messageID);
+    else {
+        var one = senderID, two = mention[0];
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "Thơm và ngon lắm hay sao mà đòi 😏", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+}
+
