e3578da82a72ede3c5dac88eb2f6825fcdc13c2a
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-06 22:25:06 +0700

    Create totinh.js

---
+++ modules/commands/totinh.js
@@ -0,0 +1,72 @@
+module.exports.config = {
+    name: "totinh",
+    version: "1.0.0",
+    hasPermssion: 0,
+    credits: "MewMew mod by VanHung, DinhPhuc, Vietdz, VĐT&NTH, Thỏadz",
+    description: "",
+    commandCategory: "Tình yêu",
+    usages: "[tag]",
+    cooldowns: 5
+};
+
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    if (!fs.existsSync(dirMaterial + "canvas")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "totinh.png")) request("https://imgur.com/AC7pnk1.jpg").pipe(fs.createWriteStream(dirMaterial + "totinh.png"));
+}
+
+async function makeImage({ one, two }) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const path = require("path");
+    const jimp = require("jimp");
+    const __root = path.resolve(__dirname, "cache", "canvas");
+    let totinh_img = await jimp.read(__root + "/totinh.png");
+    let pathImg = __root + `/totinh_${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
+    
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?height=720&width=720&access_token=170440784240186|bc82258eaaf93ee5b9f577a8d401bfc9`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+    
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+   totinh_img.resize(500, 500).composite(circleOne.resize(65, 65), 142, 86).composite(circleTwo.resize(65, 65), 293, 119);
+    
+    let raw = await totinh_img.getBufferAsync("image/png");
+    
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+    
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+
+
+module.exports.run = async function ({ event, api, args, client }) {
+    const fs = require("fs-extra");
+    let { threadID, messageID, senderID } = event;
+    var mention = Object.keys(event.mentions)[0]
+    let tag = event.mentions[mention].replace("@", "");
+    if (!mention) return api.sendMessage("Vui lòng tag 1 người", threadID, messageID);
+    else {
+        var one = senderID, two = mention;
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "Tớ thích cậu❤ " + tag + '\nLàm ny tớ nha UwU',
+            mentions: [{
+          tag: tag,
+          id: mention
+        }],
+     attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+}
