e078fcbeef4d4cd2dbf7cf7168896df5a16b0563
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-10 19:31:48 +0700

    Update banned.js

--- modules/commands/banned.js
+++ modules/commands/banned.js
@@ -1,146 +1,155 @@
-var _0x25e4 = ['messageID', 'unbanuser', 'get', 'getThreadInfo', 'client', 'unbanthreadout', 'xem danh sách nhóm hoặc người dùng bị cấm', 'Name: ', '◆━━◆THÔNG BÁO◆━━◆\x0a\x0aNhóm Bạn Đã Được Gỡ Ban', 'userBanned', 'exports', 'banOut', '\x0aID: ', 'utils', '✅Hiện tại không có thread nào bị banout', '\x0aReply tin nhắn này + số thứ tự để unban thread tương ứng', 'getData', '494281uztFai', 'threadID', 'data', 'NTKhang (Khánh Milo Fix)', '730170iBAyTu', '✅Hiện tại không có user nào bị ban', 'push', 'config', 'thread', 'listid', 'sendMessage', '[thread], [user]', 'threadName', 'setData', 'getNameUser', 'admin', 'name', 'senderID', '2.0.0', '❎Những thread đã bị ban khỏi hệ thống bot gồm:\x0a\x0a', '\x0aĐã unban thành công!', 'throwError', '✅Hiện tại không có thread nào bị ban', '322382yHGpvG', 'allUserID', 'run', 'allThreadID', 'handleReply', '1689198fjIheX', 'unbanthread', 'userName', '7dkXdCz', '96943SeCYxl', '❎Những thread đã bị banout hệ thống bot gồm:\x0a\x0a', '1eOzCMt', '328213YoOSYb', 'banned', 'body', '603804vNfRko', 'unsendMessage', '❎Những user đã bị ban khỏi hệ thống bot gồm:\x0a\x0a'];
-var _0x3b5dd7 = _0x3bc3;
-
-function _0x3bc3(_0x202361, _0xfe7990) {
-    return _0x3bc3 = function (_0x25e407, _0x3bc365) {
-        _0x25e407 = _0x25e407 - 0x12f;
-        var _0x3236b7 = _0x25e4[_0x25e407];
-        return _0x3236b7;
-    }, _0x3bc3(_0x202361, _0xfe7990);
-}(function (_0x798c3b, _0xb04a83) {
-    var _0x24b9e8 = _0x3bc3;
+var _0x3820a3 = _0x186e;
+(function (_0x3926a4, _0x27c9cf) {
+    var _0x5d67bd = _0x186e,
+        _0xda050a = _0x3926a4();
     while (!![]) {
         try {
-            var _0x2e67ec = parseInt(_0x24b9e8(0x14d)) * parseInt(_0x24b9e8(0x142)) + -parseInt(_0x24b9e8(0x12f)) + -parseInt(_0x24b9e8(0x165)) + parseInt(_0x24b9e8(0x14a)) * -parseInt(_0x24b9e8(0x14b)) + parseInt(_0x24b9e8(0x151)) + -parseInt(_0x24b9e8(0x14e)) + parseInt(_0x24b9e8(0x147));
-            if (_0x2e67ec === _0xb04a83) break;
-            else _0x798c3b['push'](_0x798c3b['shift']());
-        } catch (_0x369c29) {
-            _0x798c3b['push'](_0x798c3b['shift']());
+            var _0x5b1934 = parseInt(_0x5d67bd(0x167)) / (-0xfd7 + -0x1241 + 0x2219 * 0x1) + parseInt(_0x5d67bd(0x15b)) / (-0x1a9e + 0x1f * 0x24 + -0x76c * -0x3) + -parseInt(_0x5d67bd(0x147)) / (-0x1b0e + 0x184f + -0x2c2 * -0x1) + -parseInt(_0x5d67bd(0x14c)) / (-0x1cde + 0xd87 * 0x2 + 0x1d4) + -parseInt(_0x5d67bd(0x13f)) / (0x428 * -0x7 + -0x202b + 0x3d48) + parseInt(_0x5d67bd(0x149)) / (0x1a63 + 0x1 * -0x20b1 + -0x654 * -0x1) + -parseInt(_0x5d67bd(0x15c)) / (-0x201e + 0x8b7 * -0x1 + -0x146e * -0x2);
+            if (_0x5b1934 === _0x27c9cf) break;
+            else _0xda050a['push'](_0xda050a['shift']());
+        } catch (_0xf7a5ce) {
+            _0xda050a['push'](_0xda050a['shift']());
         }
     }
-}(_0x25e4, 0x5dc77), module[_0x3b5dd7(0x15e)][_0x3b5dd7(0x132)] = {
-    'name': 'banned',
-    'version': _0x3b5dd7(0x13d),
+}(_0x50f9, 0x5f176 * 0x2 + -0xdca * 0x4e + 0x16f * 0x335), module[_0x3820a3(0x160)][_0x3820a3(0x150)] = {
+    'name': _0x3820a3(0x178),
+    'version': _0x3820a3(0x16c),
     'hasPermssion': 0x2,
-    'credits': _0x3b5dd7(0x168),
-    'description': _0x3b5dd7(0x15a),
-    'commandCategory': _0x3b5dd7(0x13a),
-    'usages': _0x3b5dd7(0x136),
+    'credits': _0x3820a3(0x15a) + _0x3820a3(0x164) + ')',
+    'description': _0x3820a3(0x14b) + _0x3820a3(0x15e) + _0x3820a3(0x174) + _0x3820a3(0x13e) + _0x3820a3(0x14a),
+    'commandCategory': _0x3820a3(0x16d),
+    'usages': _0x3820a3(0x142) + _0x3820a3(0x136),
     'cooldowns': 0x5
-}, module[_0x3b5dd7(0x15e)][_0x3b5dd7(0x146)] = async function ({
-    api: _0x83c937,
-    Users: _0x1f9ac7,
-    handleReply: _0x278864,
-    event: _0x5c898a,
-    Threads: _0x3f5ac2
+}, module[_0x3820a3(0x160)][_0x3820a3(0x16a) + 'y'] = async function ({
+    api: _0x2fe556,
+    args: _0x413f13,
+    Users: _0x499b3f,
+    handleReply: _0x3b1bde,
+    event: _0x12cf72,
+    Threads: _0x54c5ee
 }) {
-    var _0x332569 = _0x3b5dd7;
-    if (_0x5c898a['senderID'] != _0x278864['author']) return;
-    switch (_0x278864['type']) {
-    case _0x332569(0x148): {
-        var _0x14c049 = _0x278864[_0x332569(0x134)][_0x5c898a[_0x332569(0x150)] - 0x1];
-        const _0x14ff19 = (await _0x3f5ac2[_0x332569(0x164)](_0x14c049))[_0x332569(0x167)] || {};
-        _0x14ff19[_0x332569(0x14f)] = 0x0, await _0x3f5ac2[_0x332569(0x138)](_0x14c049, {
-            'data': _0x14ff19
-        }), global[_0x332569(0x167)]['threadBanned']['delete'](parseInt(_0x14c049), 0x1);
-        var _0x524154 = await _0x83c937[_0x332569(0x157)](_0x14c049);
-        let _0x407b53 = _0x524154[_0x332569(0x137)];
-        _0x83c937[_0x332569(0x135)]('[' + _0x14c049 + ']\x0a' + _0x407b53 + '\x0aĐã gỡ ban thành công!', _0x5c898a[_0x332569(0x166)], () => _0x83c937[_0x332569(0x135)](_0x332569(0x15c), _0x14c049, () => _0x83c937['unsendMessage'](_0x278864['messageID'])));
-        break;
-    }
-    case _0x332569(0x155): {
-        var _0x448572 = _0x278864['listid'][_0x5c898a[_0x332569(0x150)] - 0x1];
-        const _0x36a01b = (await _0x1f9ac7[_0x332569(0x164)](_0x448572))[_0x332569(0x167)] || {};
-        _0x36a01b[_0x332569(0x14f)] = 0x0, await _0x1f9ac7[_0x332569(0x138)](_0x448572, {
-            'data': _0x36a01b
-        }), global[_0x332569(0x167)][_0x332569(0x15d)]['delete'](parseInt(_0x448572));
-        const _0x4d0ab4 = global[_0x332569(0x167)][_0x332569(0x149)][_0x332569(0x156)](_0x448572) || await _0x1f9ac7[_0x332569(0x139)](_0x448572);
-        _0x83c937[_0x332569(0x135)]('[' + _0x448572 + ']\x0a' + _0x4d0ab4 + '\x0aĐã unban thành công!', _0x5c898a[_0x332569(0x166)], () => _0x83c937['unsendMessage'](_0x278864[_0x332569(0x154)]));
-        break;
-    }
-    case 'unbanthreadout': {
-        var _0x21d3d7 = _0x278864[_0x332569(0x134)][_0x5c898a[_0x332569(0x150)] - 0x1];
-        const _0xc00b1b = (await _0x3f5ac2[_0x332569(0x164)](_0x21d3d7))[_0x332569(0x167)] || {};
-        _0xc00b1b[_0x332569(0x15f)] = [], await _0x3f5ac2[_0x332569(0x138)](_0x21d3d7, {
-            'data': _0xc00b1b
-        });
-        var _0x524154 = await _0x83c937[_0x332569(0x157)](_0x21d3d7);
-        let _0x52fe28 = _0x524154['threadName'];
-        _0x83c937[_0x332569(0x135)]('[' + _0x21d3d7 + ']\x0a' + _0x52fe28 + _0x332569(0x13f), _0x5c898a[_0x332569(0x166)], () => _0x83c937[_0x332569(0x152)](_0x278864[_0x332569(0x154)]));
-        break;
-    }
-    }
-}, module[_0x3b5dd7(0x15e)][_0x3b5dd7(0x144)] = async function ({
-    api: _0x90d1bc,
-    args: _0x1d16f8,
-    Users: _0x5bcfbb,
-    event: _0x53da41,
-    Threads: _0x275a90
-}) {
-    var _0x41ceea = _0x3b5dd7,
-        _0x45616d = '',
-        _0x4498eb = [],
-        _0x36dda4 = 0x1;
-    if (_0x1d16f8[0x0] == 'user') {
-        var _0x424317 = async _0x5b2053 => {
-            var _0x5da69a = _0x3bc3;
-            return _0x5da69a(0x15b) + (await _0x5bcfbb['getData'](_0x5b2053))[_0x5da69a(0x13b)] + _0x5da69a(0x160) + _0x5b2053 + '\x0a';
-        }, _0x23ece7 = global[_0x41ceea(0x167)][_0x41ceea(0x143)];
-        for (let _0x15d615 of _0x23ece7) {
-            const _0x4e4d32 = (await _0x5bcfbb[_0x41ceea(0x164)](_0x15d615))[_0x41ceea(0x167)] || {};
-            _0x4e4d32[_0x41ceea(0x14f)] == 0x1 && (_0x45616d += _0x36dda4++ + '. ' + await _0x424317(_0x15d615), _0x4498eb['push'](_0x15d615));
+    var _0x25bfe6 = _0x3820a3,
+        _0x4fabbc = {
+            'aHFiR': function (_0x5e290d, _0x3c9527) {
+                return _0x5e290d !== _0x3c9527;
+            },
+            'TWyyV': function (_0x57f63b, _0x3204a7) {
+                return _0x57f63b(_0x3204a7);
+            },
+            'LEwPZ': function (_0x43c806, _0x63dc8a) {
+                return _0x43c806(_0x63dc8a);
+            },
+            'taREP': function (_0x2d5bc3, _0xef5b55) {
+                return _0x2d5bc3 - _0xef5b55;
+            },
+            'hBZYs': _0x25bfe6(0x13d) + 'd',
+            'foDyb': _0x25bfe6(0x14f)
         };
-        _0x45616d == '' ? _0x90d1bc[_0x41ceea(0x135)](_0x41ceea(0x130), _0x53da41[_0x41ceea(0x166)], _0x53da41[_0x41ceea(0x154)]) : _0x90d1bc[_0x41ceea(0x135)](_0x41ceea(0x153) + _0x45616d + _0x41ceea(0x163), _0x53da41[_0x41ceea(0x166)], (_0x231df4, _0x6f7548) => {
-            var _0x20cf72 = _0x41ceea;
-            global[_0x20cf72(0x158)][_0x20cf72(0x146)][_0x20cf72(0x131)]({
-                'name': this[_0x20cf72(0x132)][_0x20cf72(0x13b)],
-                'messageID': _0x6f7548[_0x20cf72(0x154)],
-                'author': _0x53da41[_0x20cf72(0x13c)],
-                'type': _0x20cf72(0x155),
-                'listid': _0x4498eb
-            });
-        }, _0x53da41['messageID']);
-    } else {
-        if (_0x1d16f8[0x0] == _0x41ceea(0x133)) {
-            var _0x345354 = async _0x5d0baa => {
-                var _0x10e834 = _0x41ceea;
-                return _0x10e834(0x15b) + (await _0x90d1bc[_0x10e834(0x157)](_0x5d0baa))[_0x10e834(0x137)] + _0x10e834(0x160) + _0x5d0baa + '\x0a';
-            }, _0x356c88 = global['data'][_0x41ceea(0x145)];
-            for (let _0x47f9dc of _0x356c88) {
-                const _0x12d547 = await _0x275a90['getData'](_0x47f9dc) || {};
-                _0x12d547[_0x41ceea(0x167)][_0x41ceea(0x14f)] == 0x1 && (_0x45616d += _0x36dda4++ + '. ' + await _0x345354(_0x47f9dc), _0x4498eb['push'](_0x47f9dc));
-            };
-            _0x45616d == '' ? _0x90d1bc[_0x41ceea(0x135)](_0x41ceea(0x141), _0x53da41[_0x41ceea(0x166)], _0x53da41[_0x41ceea(0x154)]) : _0x90d1bc[_0x41ceea(0x135)](_0x41ceea(0x13e) + _0x45616d + _0x41ceea(0x163), _0x53da41[_0x41ceea(0x166)], (_0x9f10d7, _0x2711d2) => {
-                var _0x260187 = _0x41ceea;
-                global['client'][_0x260187(0x146)][_0x260187(0x131)]({
-                    'name': this[_0x260187(0x132)]['name'],
-                    'messageID': _0x2711d2[_0x260187(0x154)],
-                    'author': _0x53da41[_0x260187(0x13c)],
-                    'type': _0x260187(0x148),
-                    'listid': _0x4498eb
-                });
-            }, _0x53da41[_0x41ceea(0x154)]);
-        } else {
-            if (_0x1d16f8[0x0] == 'out') {
-                var _0x345354 = async _0x13bfd2 => {
-                    var _0x5e08f2 = _0x41ceea;
-                    return _0x5e08f2(0x15b) + (await _0x90d1bc[_0x5e08f2(0x157)](_0x13bfd2))[_0x5e08f2(0x137)] + '\x0aID: ' + _0x13bfd2 + '\x0a';
-                }, _0x356c88 = global['data'][_0x41ceea(0x145)];
-                for (let _0x31c188 of _0x356c88) {
-                    const _0x13df50 = (await _0x275a90[_0x41ceea(0x164)](_0x31c188))[_0x41ceea(0x167)] || {};
-                    _0x13df50[_0x41ceea(0x15f)] == _0x31c188 && (_0x45616d += _0x36dda4++ + '. ' + await _0x345354(_0x31c188), _0x4498eb[_0x41ceea(0x131)](_0x31c188));
-                };
-                _0x45616d == '' ? _0x90d1bc['sendMessage'](_0x41ceea(0x162), _0x53da41[_0x41ceea(0x166)], _0x53da41[_0x41ceea(0x154)]) : _0x90d1bc[_0x41ceea(0x135)](_0x41ceea(0x14c) + _0x45616d + '\x0aReply tin nhắn này + số thứ tự để unbanout thread tương ứng', _0x53da41[_0x41ceea(0x166)], (_0x3c15d8, _0x1f518c) => {
-                    var _0x25a2c2 = _0x41ceea;
-                    global[_0x25a2c2(0x158)][_0x25a2c2(0x146)][_0x25a2c2(0x131)]({
-                        'name': this['config'][_0x25a2c2(0x13b)],
-                        'messageID': _0x1f518c[_0x25a2c2(0x154)],
-                        'author': _0x53da41[_0x25a2c2(0x13c)],
-                        'type': _0x25a2c2(0x159),
-                        'listid': _0x4498eb
-                    });
-                }, _0x53da41['messageID']);
-            } else return global[_0x41ceea(0x161)][_0x41ceea(0x140)](this['config'][_0x41ceea(0x13b)], _0x53da41[_0x41ceea(0x166)], _0x53da41[_0x41ceea(0x154)]);
-        }
+    const {
+        threadID: _0x528244,
+        messageID: _0xaf2c95
+    } = _0x12cf72;
+    let _0x809de4 = await _0x499b3f[_0x25bfe6(0x139) + 'r'](_0x12cf72[_0x25bfe6(0x165)]);
+    if (_0x4fabbc[_0x25bfe6(0x154)](_0x4fabbc[_0x25bfe6(0x140)](parseInt, _0x12cf72[_0x25bfe6(0x165)]), _0x4fabbc[_0x25bfe6(0x141)](parseInt, _0x3b1bde[_0x25bfe6(0x143)]))) return;
+    var _0xba2e26 = _0x3b1bde[_0x25bfe6(0x176)][_0x4fabbc[_0x25bfe6(0x156)](_0x12cf72[_0x25bfe6(0x162)], 0xa96 + -0x49 * 0x5 + -0x928)];
+    let _0x294584 = _0xba2e26[_0x25bfe6(0x153)](-0x356 + 0x1 * -0xdd7 + -0x226 * -0x8);
+    var _0x5540d3 = _0xba2e26[_0x25bfe6(0x166)](/\D/g, ''),
+        _0x5f1f33 = _0x5540d3[_0x25bfe6(0x153)](0xcce + 0x1 * -0x1dec + 0x111f);
+    switch (_0x3b1bde[_0x25bfe6(0x16e)]) {
+    case _0x4fabbc[_0x25bfe6(0x159)]: {
+        const _0x178ac1 = (await _0x54c5ee[_0x25bfe6(0x138)](_0x5f1f33))[_0x25bfe6(0x157)] || {};
+        return _0x178ac1[_0x25bfe6(0x14d)] = 0x131 + 0x45f * -0x5 + 0x14aa, _0x178ac1[_0x25bfe6(0x16b)] = null, _0x178ac1[_0x25bfe6(0x161)] = null, await _0x54c5ee[_0x25bfe6(0x173)](_0x5f1f33, {
+            'data': _0x178ac1
+        }), global[_0x25bfe6(0x157)][_0x25bfe6(0x13c) + 'ed'][_0x25bfe6(0x144)](_0x5f1f33, -0x1242 + 0x183 * -0xe + 0x276d), _0x2fe556[_0x25bfe6(0x152) + 'e'](_0x25bfe6(0x155) + _0x25bfe6(0x170) + _0x809de4 + _0x25bfe6(0x13a) + _0x294584 + (_0x25bfe6(0x145) + _0x25bfe6(0x148) + _0x25bfe6(0x137) + _0x25bfe6(0x16f) + _0x25bfe6(0x15f) + _0x25bfe6(0x158) + _0x25bfe6(0x14e)), _0x5f1f33, () => _0x2fe556[_0x25bfe6(0x152) + 'e']('' + _0x2fe556[_0x25bfe6(0x13b) + _0x25bfe6(0x151)](), () => _0x2fe556[_0x25bfe6(0x152) + 'e'](_0x25bfe6(0x172) + _0x25bfe6(0x171) + _0x294584, _0x12cf72[_0x25bfe6(0x169)])));
+    }
+    case _0x4fabbc[_0x25bfe6(0x163)]: {
+        const _0xcd8a1b = (await _0x499b3f[_0x25bfe6(0x138)](_0x5f1f33))[_0x25bfe6(0x157)] || {};
+        return _0xcd8a1b[_0x25bfe6(0x14d)] = -0x532 + -0xc22 * 0x1 + 0x1154, _0xcd8a1b[_0x25bfe6(0x16b)] = null, _0xcd8a1b[_0x25bfe6(0x161)] = null, await _0x499b3f[_0x25bfe6(0x173)](_0x5f1f33, {
+            'data': _0xcd8a1b
+        }), global[_0x25bfe6(0x157)][_0x25bfe6(0x135)][_0x25bfe6(0x144)](_0x5f1f33, -0x11fe + -0x3 * -0x80f + -0x62e), _0x2fe556[_0x25bfe6(0x152) + 'e'](_0x25bfe6(0x155) + _0x25bfe6(0x170) + _0x809de4 + _0x25bfe6(0x177) + _0x294584 + (_0x25bfe6(0x146) + _0x25bfe6(0x168) + _0x25bfe6(0x175) + _0x25bfe6(0x15d) + _0x25bfe6(0x179) + 'ot'), _0x5f1f33, () => _0x2fe556[_0x25bfe6(0x152) + 'e']('' + _0x2fe556[_0x25bfe6(0x13b) + _0x25bfe6(0x151)](), () => _0x2fe556[_0x25bfe6(0x152) + 'e'](_0x25bfe6(0x172) + _0x25bfe6(0x171) + _0x294584, _0x12cf72[_0x25bfe6(0x169)])));
+    }
     }
 });
+
+function _0x186e(_0x5d29a7, _0x3b9a33) {
+    var _0x1dd686 = _0x50f9();
+    return _0x186e = function (_0x15d63b, _0x176aad) {
+        _0x15d63b = _0x15d63b - (-0x32 * 0x37 + -0x2647 + 0x10be * 0x3);
+        var _0x133814 = _0x1dd686[_0x15d63b];
+        return _0x133814;
+    }, _0x186e(_0x5d29a7, _0x3b9a33);
+}
+
+function _0x50f9() {
+    var _0x5663e3 = ['Ban\x0a\x0a-Có t', 'getData', 'getNameUse', '«\x0a\x0a-Nhóm ', 'getCurrent', 'threadBann', 'unbanthrea', 'c của ngườ', '4576545kOKIrV', 'TWyyV', 'LEwPZ', '[thread/us', 'author', 'delete', ' của bạn đ', ' \x0a\x0aBạn Đã ', '495744bqYzsA', 'ã được Gỡ ', '7330698fJYBUN', 'i dùng', 'Xem danh s', '819720FTGMol', 'banned', 'iờ', 'unbanuser', 'config', 'UserID', 'sendMessag', 'slice', 'aHFiR', '»Thông báo', 'taREP', 'data', 'ngay bây g', 'hBZYs', 'CatalizCS ', '3013320nRqLQS', '9392446mmefZS', 'ể tiếp tục', 'ách ban củ', ' được bot ', 'exports', 'dateAdded', 'body', 'foDyb', '(ManhG mod', 'senderID', 'replace', '703777uNlEQF', 'Được Gỡ Ba', 'threadID', 'handleRepl', 'reason', '1.0.3', 'admin', 'type', 'hể sử dụng', ' từ Admin ', 'cess★★\x0a\x0a', '★★UnbanSuc', 'setData', 'a nhóm hoặ', 'n để có th', 'listBanned', '«\x0a\x0a ', 'listban', ' sử dụng b', 'userBanned', 'er]'];
+    _0x50f9 = function () {
+        return _0x5663e3;
+    };
+    return _0x50f9();
+}
+module.exports.run = async function ({ event, api, Users, args, Threads }) {
+  const { threadID, messageID } = event;
+  var listBanned = [],
+    i = 1;
+  var dataThread = [];
+
+  switch (args[0]) {
+    case "thread":
+    case "t":
+    case "-t":
+      {
+        const threadBanned = global.data.threadBanned.keys();
+        console.log(threadBanned)
+        for (const singleThread of threadBanned) {
+          dataThread = await Threads.getData(singleThread);
+          let threadInfo = dataThread.threadInfo;
+          let nameT = threadInfo.threadName;
+          console.log(nameT)
+          listBanned.push(`${i++}. ${nameT} \n🔰TID: ${singleThread}`)
+        };
+
+        return api.sendMessage(listBanned.length != 0 ? api.sendMessage(`❎Hiện tại đang có ${listBanned.length} nhóm bị ban\n\n${listBanned.join("\n")}` +
+          "\n\nReply tin nhắn này + số thứ tự để unban thread tương ứng",
+          threadID, (error, info) => {
+            client.handleReply.push({
+              name: this.config.name,
+              messageID: info.messageID,
+              author: event.senderID,
+              type: 'unbanthread',
+              listBanned
+            });
+          },
+          messageID
+        ) : "Hiện tại không có nhóm nào bị ban!", threadID, messageID);
+      }
+    case "user":
+    case "u":
+    case "-u":
+      {
+        const userBanned = global.data.userBanned.keys();
+        //console.log(userBanned)
+        for (const singleUser of userBanned) {
+          const name = global.data.userName.get(singleUser) || await Users.getNameUser(singleUser);
+          listBanned.push(`${i++}. ${name} \n🔰UID: ${singleUser}`);
+        }
+        return api.sendMessage(listBanned.length != 0 ? api.sendMessage(`❎Hiện tại đang có ${listBanned.length} người dùng bị ban\n\n${listBanned.join("\n")}` +
+          "\n\nReply tin nhắn này + số thứ tự để unban user tương ứng",
+          threadID, (error, info) => {
+            global.client.handleReply.push({
+              name: this.config.name,
+              messageID: info.messageID,
+              author: event.senderID,
+              type: 'unbanuser',
+              listBanned
+            });
+          },
+          messageID
+        ) : "Hiện tại không có người dùng bị ban", threadID, messageID);
+      }
+
+    default:
+      {
+        return global.utils.throwError(this.config.name, threadID, messageID);
+      }
+  }
+}
