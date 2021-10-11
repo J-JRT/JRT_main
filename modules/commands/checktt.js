889056f0a25f5add49b7f9150af439e49122fcee
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-11 08:47:15 +0700

    Update checktt.js

--- modules/commands/checktt.js
+++ modules/commands/checktt.js
@@ -1,9 +1,8 @@
 var limit = 20; //số thành viên mỗi lần check
-//nên xóa data.sqlite ở include và npm i ... npm audit fix để tránh bị lỗi tên undefined
 module.exports.config = {
 	name: "checktt",
 	version: "1.8.0",
-	hasPermssion: 2,
+	hasPermssion: 0,
 	credits: "Mirai Team fix get by D-Jukie",
 	description: "Kiểm tra lượt tương tác trong nhóm",
 	commandCategory: "Nhóm",
@@ -44,7 +43,9 @@ var mention = Object.keys(event.mentions);
             msg += `\n--Trang ${page}/${numPage}--\n🌻Dùng ${global.config.PREFIX}checktt all số trang🌻`
             return api.sendMessage(msg, event.threadID);
         }        
-    else if (mention[0]) {
+    else    
+    if(event.type == "message_reply") { mention[0] = event.messageReply.senderID }
+    if (mention[0]) {
             var { participantIDs } =(await Threads.getData(event.threadID)).threadInfo;
             //const countMess = (await Currencies.getData(event.senderID)).exp
             const listUserID = event.participantIDs
