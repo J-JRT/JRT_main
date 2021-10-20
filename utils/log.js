8a92f317fec6c3dc948bddf1d527bc4101a5ff9b
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-20 08:42:23 +0700

    Add files via upload

--- utils/log.js
+++ utils/log.js
@@ -3,10 +3,10 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.yellow('[ ❕ Lỗi ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			console.log(chalk.red('[ ❕ Lỗi ] » ') + data);
 			break;
 		default:
 			console.log(chalk.magenta(`${option} » `) + data);
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('[ J-JRT ] » ') + data);
+			console.log(chalk.yellow('[ OREO ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('[ J-JRT ] » ') + data);
+			console.log(chalk.red('[ OREO ] » ') + data);
 			break;
 		default:
-			console.log(chalk.green(`[ J-JRT ] » `) + data);
+			console.log(chalk.green(`[ OREO ] » `) + data);
 			break;
 	}
 }
