ac3680c5504bc233d4cf225d67a696583b3e9024
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-25 09:12:54 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,48 +1,50 @@
-//const chrome = require('chrome-cookies-secure');
-//var fs = require('fs');
-
-//chrome.getCookies('https://www.facebook.com/', 'puppeteer', function(err, cookies) {
-  //  let cooky = cookies.map(mapper => ({
-    //    key: mapper.name,
-      //  value: mapper.value,
-        //domain: "facebook.com",
-        //path: mapper.path,
-        //hostOnly: mapper.hostOnly,
-        //creation: new Date().toISOString(),
-    //    lastAccessed: new Date().toISOString()
-    //}));
-    //let appstate = JSON.stringify(cooky, null, 4);
-    //fs.writeFile('appstate.json', appstate, 'utf8', function(err) {
-      //  if (err) throw err;
-       // console.log('complete');
-    //});
-//});
-const fs = require("fs");
-const login = require("facebook-chat-api");
+const fs = require("fs-extra");
+const login = require("@miraipr0ject/fca-unofficial");
 const readline = require("readline");
+const totp = require("totp-generator");
+
+let configPath = "";
+let argv = process.argv.slice(2);
+if (argv.length !== 0) configPath = argv[0];
+else configPath = "./config.json";
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 });
 
-const obj = {email: "FB_EMAIL", password: "FB_PASSWORD"};
-login(obj, (err, api) => {
-    if(err) {
-        switch (err.error) {
-            case 'login-approval':
-                console.log('Enter code > ');
-                rl.on('line', (line) => {
-                    err.continue(line);
-                    rl.close();
-                });
-                break;
-            default:
-                console.error(err);
-        }
-        return;
-    }
+const option = {
+	logLevel: "silent",
+	forceLogin: true,
+	userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36"
+};
 
-    // Logged in!
+const config = require(`./${configPath}`);
+let email = config.EMAIL;
+let password = config.PASSWORD;
+let otpkey = config.OTPKEY.replace(/\s+/g, '').toLowerCase();
+
+login({ email, password }, option, (err, api) => {
+	if (err) {
+		switch (err.error) {
+			case "login-approval":
+				if (otpkey) err.continue(totp(otpkey));
+				else {
+					console.log("Nhập mã xác minh 2 lớp:");
+					rl.on("line", line => {
+						err.continue(line);
+						rl.close();
+					});
+				}
+				break;
+			default:
+			console.error(err);
+			process.exit(1);
+		}
+		return;
+	}
+	const json = JSON.stringify(api.getAppState());
+	fs.writeFileSync(`./${config.APPSTATEPATH}`, json);
+	console.log("Đã ghi xong appstate!");
+	process.exit(0);
 });
-
