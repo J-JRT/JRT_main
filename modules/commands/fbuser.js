727cb652d7f2903e1543732cc540cd91e271c66e
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-10-27 13:00:59 +0700

    Update fbuser.js

--- modules/commands/fbuser.js
+++ modules/commands/fbuser.js
@@ -1,1 +1,50 @@
-var _0x76661c=_0x101d;(function(_0x54bee1,_0x435b2c){var _0x15e0d8=_0x101d,_0x102013=_0x54bee1();while(!![]){try{var _0x46d0a0=parseInt(_0x15e0d8(0xdf))/0x1+-parseInt(_0x15e0d8(0xd7))/0x2*(parseInt(_0x15e0d8(0xe4))/0x3)+-parseInt(_0x15e0d8(0xdd))/0x4*(parseInt(_0x15e0d8(0xd4))/0x5)+-parseInt(_0x15e0d8(0xcc))/0x6*(parseInt(_0x15e0d8(0xd8))/0x7)+parseInt(_0x15e0d8(0xcd))/0x8*(parseInt(_0x15e0d8(0xe5))/0x9)+-parseInt(_0x15e0d8(0xe0))/0xa+parseInt(_0x15e0d8(0xd9))/0xb;if(_0x46d0a0===_0x435b2c)break;else _0x102013['push'](_0x102013['shift']());}catch(_0x17a8e1){_0x102013['push'](_0x102013['shift']());}}}(_0x1b6b,0x92b13),module[_0x76661c(0xd6)]['config']={'name':_0x76661c(0xcf),'version':'1.0.0','hasPermssion':0x1,'credits':'ProCoderMew','description':_0x76661c(0xe2),'commandCategory':'Nhóm','usages':'','cooldowns':0x1e},module['exports'][_0x76661c(0xd2)]=async function({api:_0x70f914,event:_0x363fa7}){var _0x3e490=_0x76661c,{userInfo:_0x878091,adminIDs:_0x505a71}=await _0x70f914[_0x3e490(0xd5)](_0x363fa7['threadID']),_0x125670=0x0,_0xbf848f=0x0,_0x271d11=[];for(const _0x27369b of _0x878091){_0x27369b[_0x3e490(0xdb)]==undefined&&_0x271d11[_0x3e490(0xe3)](_0x27369b['id']);};_0x505a71=_0x505a71['map'](_0x363dec=>_0x363dec['id'])[_0x3e490(0xd0)](_0x97d450=>_0x97d450==_0x70f914[_0x3e490(0xde)]());if(_0x271d11[_0x3e490(0xda)]==0x0)return _0x70f914[_0x3e490(0xdc)]('Trong\x20nhóm\x20bạn\x20không\x20tồn\x20tại\x20\x27Người\x20dùng\x20Facebook\x27.',_0x363fa7[_0x3e490(0xd1)]);else _0x70f914['sendMessage']('Nhóm\x20bạn\x20hiện\x20có\x20'+_0x271d11[_0x3e490(0xda)]+'\x20\x27Người\x20dùng\x20Facebook\x27.',_0x363fa7[_0x3e490(0xd1)],function(){var _0x2e2196=_0x3e490;!_0x505a71?_0x70f914[_0x2e2196(0xdc)](_0x2e2196(0xe1),_0x363fa7[_0x2e2196(0xd1)]):_0x70f914[_0x2e2196(0xdc)]('Bắt\x20đầu\x20lọc..',_0x363fa7[_0x2e2196(0xd1)],async function(){var _0x4f2944=_0x2e2196;for(const _0x266824 of _0x271d11){try{await new Promise(_0x3687d8=>setTimeout(_0x3687d8,0x3e8)),await _0x70f914[_0x4f2944(0xd3)](parseInt(_0x266824),_0x363fa7[_0x4f2944(0xd1)]),_0x125670++;}catch{_0xbf848f++;}}_0x70f914['sendMessage']('Đã\x20lọc\x20thành\x20công\x20'+_0x125670+'\x20người.',_0x363fa7[_0x4f2944(0xd1)],function(){var _0x3647bc=_0x4f2944;if(_0xbf848f!=0x0)return _0x70f914['sendMessage'](_0x3647bc(0xce)+_0xbf848f+'\x20người.',_0x363fa7[_0x3647bc(0xd1)]);});});});});function _0x101d(_0x137ec5,_0x579c7f){var _0x1b6b34=_0x1b6b();return _0x101d=function(_0x101d03,_0xc5821){_0x101d03=_0x101d03-0xcc;var _0x50e193=_0x1b6b34[_0x101d03];return _0x50e193;},_0x101d(_0x137ec5,_0x579c7f);}function _0x1b6b(){var _0x170088=['getCurrentUserID','644201spYCyR','6936470EQzRKy','Nhưng\x20bot\x20không\x20phải\x20là\x20quản\x20trị\x20viên\x20nên\x20không\x20thể\x20lọc\x20được.','Lọc\x20người\x20dùng\x20Facebook','push','2826681kOunrI','2093211Byglqd','688752XtAxrk','32CauxES','Lọc\x20thất\x20bại\x20','kickndfb','some','threadID','run','removeUserFromGroup','45UzzxWQ','getThreadInfo','exports','2hPYYZz','28IfODYu','24880614QjSKNu','length','gender','sendMessage','506888FMCuTj'];_0x1b6b=function(){return _0x170088;};return _0x1b6b();}
+module.exports.config = {
+	name: "kickndfb",
+	version: "1.0.0",
+	hasPermssion: 1,
+	credits: "ProCoderMew",
+	description: "Lọc người dùng Facebook",
+	commandCategory: "Nhóm",
+	usages: "",
+	cooldowns: 20
+};
+
+module.exports.run = async function({ api, event }) {
+    var { userInfo, adminIDs } = await api.getThreadInfo(event.threadID);    
+    var success = 0, fail = 0;
+    var arr = [];
+    for (const e of userInfo) {
+        if (e.gender == undefined) {
+            arr.push(e.id);
+        }
+    };
+
+    adminIDs = adminIDs.map(e => e.id).some(e => e == api.getCurrentUserID());
+    if (arr.length == 0) {
+        return api.sendMessage("Trong nhóm bạn không tồn tại 'Người dùng Facebook'.", event.threadID);
+    }
+    else {
+        api.sendMessage("Nhóm bạn hiện có " + arr.length + " 'Người dùng Facebook'.", event.threadID, function () {
+            if (!adminIDs) {
+                api.sendMessage("Nhưng bot không phải là quản trị viên nên không thể lọc được.", event.threadID);
+            } else {
+                api.sendMessage("Bắt đầu lọc..", event.threadID, async function() {
+                    for (const e of arr) {
+                        try {
+                            await new Promise(resolve => setTimeout(resolve, 1000));
+                            await api.removeUserFromGroup(parseInt(e), event.threadID);   
+                            success++;
+                        }
+                        catch {
+                            fail++;
+                        }
+                    }
+                  
+                    api.sendMessage("Đã lọc thành công " + success + " người.", event.threadID, function() {
+                        if (fail != 0) return api.sendMessage("Lọc thất bại " + fail + " người.", event.threadID);
+                    });
+                })
+            }
+        })
+    }
+}
