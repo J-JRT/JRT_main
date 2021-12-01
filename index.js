a9abad7b4dd51e4d49645d80c2f330352209c37f
J-JRT <88273015+J-JRT@users.noreply.github.com>
2021-12-01 19:38:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const axios = require("axios");
 const semver = require("semver");
 const logger = require("./utils/log");
+const { base64encode, base64decode } = require('nodejs-base64');
 
 /////////////////////////////////////////////
 //========= Check node.js version =========//
@@ -21,20 +22,20 @@ const logger = require("./utils/log");
 
 const dashboard = http.createServer(function (_req, res) {
     res.writeHead(200, "OK", { "Content-Type": "text/plain" });
-    res.write("HI! XIN CHÀO CẬU CHỦ NGUYỄN HẢI ĐĂNG ( JRT )");
+    res.write("Xin chào cậu chủ Nguyễn Hải Đăng");
     res.end();
 });
 
 dashboard.listen(process.env.port || 0);
 
-logger("Opened server site...", "Bắt đầu khởi tạo BOT JRT");
+logger("BOT JRT đã khởi tạo thành công", "BOT JRT đang khởi tạo");
 
 /////////////////////////////////////////////////////////
 //========= Create start bot and make it loop =========//
 /////////////////////////////////////////////////////////
 
 function startBot(message) {
-    (message) ? logger(message, "Bắt đầu khởi tạo BOT JRT") : "";
+    (message) ? logger(message, "BOT JRT đang khởi chạy") : "";
 
     const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "mirai.js"], {
         cwd: __dirname,
@@ -44,20 +45,62 @@ function startBot(message) {
 
     child.on("close", (codeExit) => {
         if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("Khởi động lại BOT JRT");
+            startBot("BOT JRT đã được restart");
             global.countRestart += 1;
             return;
         } else return;
     });
 
     child.on("error", function (error) {
-        logger("An error occurred: " + JSON.stringify(error), "Bắt đầu khởi tạo BOT JRT");
+        logger("An error occurred: " + JSON.stringify(error), "BOT JRT đang khởi tạo");
     });
 };
+function decode(data){
+  let decoded = base64decode(base64decode(base64decode(data).toString("utf-8").toString("utf-8").toString("utf-8")))
+return decoded
+}
 ////////////////////////////////////////////////
 //========= Check update from Github =========//
 ////////////////////////////////////////////////
 
-const _0x5894db=_0x3273;function _0x3273(_0x2ff092,_0x6f595a){const _0x3399bf=_0x3399();return _0x3273=function(_0x3273e6,_0x105057){_0x3273e6=_0x3273e6-0x15f;let _0x122902=_0x3399bf[_0x3273e6];return _0x122902;},_0x3273(_0x2ff092,_0x6f595a);}function _0x3399(){const _0x58e4f4=['get','https://raw.githubusercontent.com/d-jukie/miraiv2_fix/main/package.json','2PFcQOT','2555ptCgKO','A\x20new\x20update\x20is\x20available!\x20Open\x20terminal/cmd\x20and\x20type\x20\x22node\x20update\x22\x20to\x20update!','parse','error','cwd','72NtMCSQ','565338ZnhTSU','node','55mLrvAA','3247541UzWkAC','stdio','10NdmXwj','44076rwmcwy','autoUpdate','version','12136wqGKpo','Unable\x20to\x20update:','./package.json','inherit','then','exit','A\x20new\x20update\x20is\x20available,\x20start\x20update\x20processing...','Unable\x20to\x20check\x20update.','update.js','5559012hmCNjZ','119452ualRRF','stringify','data','3803208jsHvoa','\x20CHECK\x20UPDATE\x20','\x20UPDATE\x20'];_0x3399=function(){return _0x58e4f4;};return _0x3399();}(function(_0x40d062,_0xa37a){const _0x29c58=_0x3273,_0xe375ee=_0x40d062();while(!![]){try{const _0x417154=-parseInt(_0x29c58(0x174))/0x1*(parseInt(_0x29c58(0x15f))/0x2)+parseInt(_0x29c58(0x17b))/0x3+-parseInt(_0x29c58(0x16c))/0x4*(-parseInt(_0x29c58(0x17d))/0x5)+parseInt(_0x29c58(0x16f))/0x6+parseInt(_0x29c58(0x175))/0x7*(parseInt(_0x29c58(0x162))/0x8)+parseInt(_0x29c58(0x16b))/0x9*(parseInt(_0x29c58(0x180))/0xa)+-parseInt(_0x29c58(0x17e))/0xb*(parseInt(_0x29c58(0x17a))/0xc);if(_0x417154===_0xa37a)break;else _0xe375ee['push'](_0xe375ee['shift']());}catch(_0x416938){_0xe375ee['push'](_0xe375ee['shift']());}}}(_0x3399,0x7bb5e),axios[_0x5894db(0x172)](_0x5894db(0x173))[_0x5894db(0x166)](_0x4512d8=>{const _0x1a35fe=_0x5894db,_0x2dcd38=JSON[_0x1a35fe(0x177)](readFileSync(_0x1a35fe(0x164)));if(semver['lt'](_0x2dcd38[_0x1a35fe(0x161)],_0x4512d8[_0x1a35fe(0x16e)]['version'])){if(_0x2dcd38[_0x1a35fe(0x160)]==!![]){logger(_0x1a35fe(0x168),_0x1a35fe(0x171));const _0x40e4af={};_0x40e4af[_0x1a35fe(0x179)]=__dirname,_0x40e4af[_0x1a35fe(0x17f)]=_0x1a35fe(0x165),_0x40e4af['shell']=!![];const _0x49a11d=spawn(_0x1a35fe(0x17c),[_0x1a35fe(0x16a)],_0x40e4af);_0x49a11d['on']('exit',function(){const _0x4f1bb9=_0x1a35fe;return process[_0x4f1bb9(0x167)](0x0);}),_0x49a11d['on'](_0x1a35fe(0x178),function(_0x69397e){const _0x49f22c=_0x1a35fe;logger(_0x49f22c(0x163)+JSON[_0x49f22c(0x16d)](_0x69397e),_0x49f22c(0x170));});}else logger(_0x1a35fe(0x176),_0x1a35fe(0x171)),startBot();}else logger('Bạn\x20đang\x20sử\x20dụng\x20phiên\x20bản\x20mới\x20nhất\x20của\x20JRT!!!','\x20CHECK\x20UPDATE\x20'),startBot();})['catch'](_0x59f39f=>logger(_0x5894db(0x169),_0x5894db(0x170))));
+axios.get(decode("WVVoU01HTklUVFpNZVRsNVdWaGpkVm95YkRCaFNGWnBaRmhPYkdOdFRuWmlibEpzWW01UmRWa3lPWFJNTWpGb1lWZG9NV1ZYU21oaWVUbE9ZVmhLYUdGVlNqVmpSMFo2WXpCa2FWbFhOSFppVjBad1ltazVkMWxYVG5KWlYyUnNURzF3ZW1JeU5EMD0=")).then((res) => {
+    const _0x3a9e = ['1AXwrul', 'nhật mới nhất....', 'quy trình', 'lable, sta', ' khởi tạo!!!', ' công. ', 'Bắt đầu xử lí ', 'rt update ', '862159qoBWCB', 'You are us', 'nâng cấp ', 'thành', 'update.js', 'Bản cập nhật', 'ing the la', 'autoUpdate', '958778LHuBmX', 'on!', '83620gADsGo', './package.', 'stdio', 'Đang kiểm tra bản cập', 'processing', '191jFgcNd', 'đã được', 'cwd', '18537oEWTvD', '12247HXxipe', 'Unable to ', 'parse', 'version', '1543102iuYYuT', 'inherit', 'node', 'test versi', 'exit', '274241cWRKsD'];
+const _0x4d57a7 = _0x3e46;
+(function (_0x1138e2, _0x16800a) {
+    const _0x3181c3 = _0x3e46;
+    while (!![]) {
+        try {
+            const _0x150159 = -parseInt(_0x3181c3(0xe9)) * -parseInt(_0x3181c3(0xde)) + -parseInt(_0x3181c3(0xe8)) + parseInt(_0x3181c3(0xcc)) + parseInt(_0x3181c3(0xd4)) + parseInt(_0x3181c3(0xe3)) + parseInt(_0x3181c3(0xd6)) + -parseInt(_0x3181c3(0xdf)) * parseInt(_0x3181c3(0xdb));
+            if (_0x150159 === _0x16800a) break;
+            else _0x1138e2['push'](_0x1138e2['shift']());
+        } catch (_0x5939da) {
+            _0x1138e2['push'](_0x1138e2['shift']());
+        }
+    }
+}(_0x3a9e, -0xb70ff + 0x4 * -0x15f48 + 0x6901 * 0x49));
+const local = JSON[_0x4d57a7(0xe1)](readFileSync(_0x4d57a7(0xd7) + 'json'));
 
-// THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
+function _0x3e46(_0x3db7ad, _0x121e29) {
+    return _0x3e46 = function (_0x4a7d2e, _0x50c6a7) {
+        _0x4a7d2e = _0x4a7d2e - (0xb * -0x1fc + -0x5 * -0x1e7 + 0xd1d);
+        let _0x4068ec = _0x3a9e[_0x4a7d2e];
+        return _0x4068ec;
+    }, _0x3e46(_0x3db7ad, _0x121e29);
+}
+if (semver['lt'](local[_0x4d57a7(0xe2)], res['data'][_0x4d57a7(0xe2)])) {
+    if (local[_0x4d57a7(0xd3)] == !![]) {
+        logger('A new upda' + _0x4d57a7(0xdc) + _0x4d57a7(0xec) + _0x4d57a7(0xf0) + _0x4d57a7(0xda) + '...', _0x4d57a7(0xd9) + 'nhật mới nhất');
+        const _0x50c6a7 = {};
+        _0x50c6a7[_0x4d57a7(0xdd)] = __dirname, _0x50c6a7[_0x4d57a7(0xd8)] = _0x4d57a7(0xe4), _0x50c6a7['shell'] = !![];
+        const child = spawn(_0x4d57a7(0xe5), [_0x4d57a7(0xd0)], _0x50c6a7);
+        child['on'](_0x4d57a7(0xe7), function () {
+            return process['exit'](-0x7 * -0x1f5 + -0x9f7 * 0x3 + 0x1 * 0x1032);
+        }), child['on']('error', function (_0x376882) {
+            const _0x379cf2 = _0x4d57a7;
+            logger(_0x379cf2(0xe0) + 'update: ' + JSON['stringify'](_0x376882), 'Đang kiểm tra bản cập' + _0x379cf2(0xea));
+        });
+    } else logger(_0x4d57a7(0xd1) + ' đã được ' + _0x4d57a7(0xce) + _0x4d57a7(0xcf) + _0x4d57a7(0xee) + _0x4d57a7(0xef) + _0x4d57a7(0xeb) + _0x4d57a7(0xed), _0x4d57a7(0xd9) + ' nhật mới nhất'), startBot();
+} else logger(_0x4d57a7(0xcd) + _0x4d57a7(0xd2) + _0x4d57a7(0xe6) + _0x4d57a7(0xd5), _0x4d57a7(0xd9) + _0x4d57a7(0xea)), startBot();
+}).catch(err => logger("Không thể kiểm tra bản cập nhật", "Đang kiểm tra bản cập nhật mới nhất"));
+
+//THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
