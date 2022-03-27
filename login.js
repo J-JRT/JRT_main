f554e8912230ac91b0e5cf20fbdb3e7b3232158a
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-03-27 12:07:58 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const login = require("fca-horizon-remake");
+
+var credentials = {email: "FB_EMAIL", password: "FB_PASSWORD"}; // thông tin tk
+
+login(credentials, (err, api) => {
+    if(err) return console.error(err);
+    // đăng nhập
+    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState())); //tạo appstate
+});
