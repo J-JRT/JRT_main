620520f454f5f0803629487e6aef5df814481d25
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-04-05 14:43:07 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -3,13 +3,13 @@ const chalk = require('chalk');
 module.exports = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('» Lỗi rồi «  ') + data);
+			console.log(chalk.bold.hex("#FF7F50")('[ WARN ] → ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('» Lỗi rồi «  ') + data);
+			console.log(chalk.bold.hex("#FF0000")('[ WARN ] → ') + data);
 			break;
 		default:
-			console.log(chalk.magenta(`» ${option} « `) + data);
+			console.log(chalk.bold.hex("#FF4500")(`[ ${option} ] → `) + data);
 			break;
 	}
 }
@@ -17,13 +17,13 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.yellow('» J-JRT «  ') + data);
+			console.log(chalk.bold.hex("#FF0000")('[ J-JRT ] → ') + data);
 			break;
 		case "error":
-			console.log(chalk.red('» J-JRT «  ') + data);
+			console.log(chalk.bold.hex("#FF0000")('[ J-JRT ] → ') + data);
 			break;
 		default:
-			console.log(chalk.green(`» J-JRT «  `) + data);
+			console.log(chalk.bold.hex("#00FFFF")(`[ J-JRT ] → `) + data);
 			break;
 	}
 }
