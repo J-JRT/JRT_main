d43a3731caa97be46033f96d60b5d67c15bf5f8d
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-04-05 15:12:20 +0700

    Create check.js

---
+++ modules/commands/check.js
@@ -0,0 +1,220 @@
+const axios = require("axios");
+module.exports.config = {
+	name: "check",
+	version: "0.0.1-beta",
+	hasPermssion: 0,
+	credits: "Adonis",
+	description: "Kiểm tra thông tin",
+	commandCategory: "Nhóm",
+	usages: "check",
+	cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": ""
+    }
+};
+
+module.exports.run = async ({ args, api, event, Currencies, client, Threads, Users }) => {
+   const { threadID, senderID, messageID, type, mentions } = event;
+   const moment = require("moment-timezone");
+    var timeNow = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss")
+   if (args.length == 0) return api.sendMessage(`===  Bạn có thể dùng  ===\n--------\n[⚜️] => check checkpoint => Lọc thành viên bị bay acc khỏi nhóm\n--------\n[⚜️] => check del => Lọc thành viên khỏi nhóm theo số tin nhắn\n--------\n[⚜️] => check online => Xem thời gian hoạt động bot online \n--------\n[⚜️] => check tuongtac => Check độ tương tác của bạn\n--------\n[⚜️] => check thread => Lọc nhóm dưới 30 thành viên\n--------\n[⚜️] => check all => Check tất cả độ tương tác các thành viên trong box\n[⚜️] => check user => Lọc cá \n--------\n[⚜️] => check covid => Xem thông tin covid\n--------\n[⚜️] => check luckymayman => Xem Tỉ lệ % may mắn của bạn?\n--------\n[⚜️] => check nude => Check những ảnh gợi cảm\n--------\n=== 「${timeNow}」 ===`, event.threadID, event.messageID);
+    var arr = [];
+    var mention = Object.keys(event.mentions);
+    const data = await api.getThreadInfo(event.threadID);
+    if (args[0] == "checkpoint") {// kick người dùng fb
+      if (event.senderID != 100033478361032) return api.sendMessage(`[❗] Chúc bạn may mắn lần sau:))`, event.threadID, event.messageID)
+    const find = data.adminIDs.find(el => el.id == event.senderID && api.getCurrentUserID());
+
+    if (!find) return api.sendMessage(`[⚜️] Bạn và bot cần là quản trị viên!`, event.threadID);
+    let storage = [];
+    for (const value of data.userInfo) storage.push({ "id": value.id, "gender": value.gender });
+    for (const user of storage) {
+      if (user.gender == undefined) api.removeUserFromGroup(user.id, event.threadID)
+    } return;
+
+    }  else if (args[0] == "del") {// lọc thành viên theo số tin nhắn bạn cần
+      if (event.senderID != 100033478361032) return api.sendMessage(`[❗] Chúc bạn may mắn lần sau:))`, event.threadID, event.messageID)
+    const find = data.adminIDs.find(el => el.id == event.senderID && api.getCurrentUserID());
+    if (!find) return api.sendMessage(`[⚜️] => Bạn và bot cần là quản trị viên!`,event.threadID);
+    if (!args[1]) return api.sendMessage(`[⚜️] => HDSD: check del => số tin nhắn cần lọc `,event.threadID);
+      let storage = [];
+        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+        for (const user of storage) {
+            const countMess = (await Currencies.getData(user.id)).exp;
+            if (typeof countMess == "undefined") await Currencies.setEXP(mention, parseInt(0))
+           // if (countMess ==  undefined) api.removeUserFromGroup(user.id,event.threadID)
+            if (countMess <= args[1]) setTimeout(function() { api.removeUserFromGroup(user.id,event.threadID) }, 2000);
+        } return;
+      
+    }else if (args[0] == "covid") {
+      const axios_1 = require("axios");
+  const moment = require("moment-timezone");
+  var time = moment.tz("Asia/Ho_Chi_Minh").format("YYYY");
+   let fetchdata = await axios_1.get("https://static.pipezero.com/covid/data.json");
+  var jsondata = (await fetchdata.data).total;
+  var vn = (await fetchdata.data).overview[6];
+  var year = vn.date + '-' + time;
+  var world = jsondata.world,
+    nhiemtg = world.cases,
+    chettg = world.death,
+    hoiphuctg = world.recovered,
+    //////////////////////////////
+    nhiemvn = vn.cases,
+    chetvn = vn.death,
+    hoiphucvn = vn.recovered,
+    dieutrivn = vn.treating,
+    //////////////////////////////
+    nhiemvn7days = vn.avgCases7day,
+    hoiphucvn7days = vn.avgRecovered7day,
+    chetvn7days = vn.avgDeath7day,
+    //////////////////////////////
+    ptchetvn = Math.round((chetvn * 100) / nhiemvn),
+    pthoiphucvn = Math.round((hoiphucvn * 100) / nhiemvn),
+    ptchettg = Math.round((chettg * 100) / nhiemtg),
+    pthoiphuctg = Math.round((hoiphuctg * 100) / nhiemtg),
+    pthoiphucvn = pthoiphucvn.toString().split(".")[0],
+    ptdieutrivn = (100 - pthoiphucvn - ptchetvn).toString().split(".")[0];
+  /////////////////////////////////
+  ptchetvn = ptchetvn.toString().split(".")[0];
+  pthoiphuctg = pthoiphuctg.toString().split(".")[0];
+  ptchettg = ptchettg.toString().split(".")[0];
+  return api.sendMessage(
+    "====== Thế Giới ======\n" +
+    `😷 Nhiễm: ${nhiemtg}\n` +
+    `💚 Hồi phục: ${hoiphuctg} (${pthoiphuctg}%)\n` +
+    `💀 Tử vong: ${chettg} (${ptchettg}%)\n` +
+    "====== Việt Nam ======\n" +
+    `😷 Nhiễm: ${nhiemvn}\n` +
+    `💉 Đang điều trị: ${dieutrivn} (${ptdieutrivn}%)\n` +
+    `💚 Hồi phục: ${hoiphucvn} (${pthoiphucvn}%)\n` +
+    `💀 Tử vong: ${chetvn} (${ptchetvn}%)\n` +
+    `🤨 Nhiễm 7 ngày: ${nhiemvn7days}\n` +
+    `❤ Hồi phục 7 ngày: ${hoiphucvn7days}\n` +
+    `☠️ Tử vong 7 ngày: ${chetvn7days}\n\n` +
+    //`Tin tức: ${news.vietnam}\n` +
+    `Cập nhật: ${year}`,
+    event.threadID, event.messageID
+  );
+}
+    else   if (args[0] == "user"){
+      if (event.senderID != 100033478361032) return api.sendMessage(`[❗] Chúc bạn may mắn lần sau:))`, event.threadID, event.messageID)
+            let number = [];
+            let uidAll = await Currencies.getAll(['userID','exp']);
+            uidAll.forEach(user => {
+            if(user.exp > 1) return;
+                Users.delData(user.userID);
+                Currencies.delData(user.userID);
+                number.push(user.userID);
+            })
+            return api.sendMessage(`[⚜️] Đã lọc ${number.length} cá cảnh.`,threadID);
+    }
+      else if (args[0] == "thread"){
+        if (event.senderID != 100033478361032) return api.sendMessage(`[❗] Chúc bạn may mắn lần sau:))`, event.threadID, event.messageID)
+            //let number = [];
+            api.getThreadList(50, null, ["INBOX"], (err, list) => getInfo({ list }))
+            api.getThreadList(50, null, ["OTHER"], (err, list) => getInfo({ list }))
+            api.getThreadList(50, null, ["PENDING"], (err, list) => getInfo({ list }))
+            api.getThreadList(50, null, ["unread"], (err, list) => getInfo({ list }))
+            var getInfo = ({ list }) => {
+              list.forEach(info => {
+                if (info.name == "" || info.participants < 20 || info.imageSrc == null) { 
+                  //number.push(info);
+                  api.removeUserFromGroup(api.getCurrentUserID(),info.threadID);
+                  api.deleteThread(info.threadID, (err) => {
+                    Threads.delData(info.threadID)
+                    if(err) return console.error(err);
+                    });
+                }
+              })
+            }
+           return api.sendMessage(`[⚜️] Đã lọc những nhóm không tên hoặc dưới 30 thành viên.`,threadID)
+}
+
+    else if (args[0] == "online") {
+      	let time = process.uptime();
+	let hours = Math.floor(time / (60 * 60));
+	let minutes = Math.floor((time % (60 * 60)) / 60);
+	let seconds = Math.floor(time % 60);
+      const timeStart = Date.now();
+       return api.sendMessage('[⚜️] => Đang kiểm tra kết nối vui lòng chờ... !', event.threadID, (err, info) => {
+    setTimeout(() => {
+      api.sendMessage(`[⚜️] => Ping: ${(Date.now() - timeStart)}ms\n[⚜️] => Thời gian bot hoạt động: ${hours}:${minutes}:${seconds}`, event.threadID, event.messageID);
+    }, 200);
+  }, event.messageID);
+} else if (args[0] == "lucky") {
+const request = global.nodemodule["request"];
+    const fs = global.nodemodule["fs-extra"];
+    var tile = Math.floor(Math.random() * 101); 
+    var link = ["https://i.imgur.com/gWACvcO.jpg",
+    "https://i.imgur.com/mpHit7i.jpg",
+    "https://i.imgur.com/glHFetf.jpg",
+    "https://i.imgur.com/CxwzNMv.png",
+    "https://i.imgur.com/RVerKnc.jpg"
+    ];
+var callback = () => api.sendMessage({body:`[⚜️] Tỉ lệ may mắn của bạn là ${tile}%`, attachment: fs.createReadStream(__dirname + "/cache/tile.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/tile.jpg")); 
+       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/tile.jpg")).on("close",() => callback());
+ }
+    else if (args[0] == "nude") {
+  var linkanh =  event.messageReply.attachments[0].url || args.join(" ");
+	if(!linkanh) return api.sendMessage('Vui lòng reply hoặc nhập link 1 hình ảnh!!!', event.threadID, event.messageID)
+const res = await axios.get(`https://api-rosie.j-jrt-official.repl.co/checknude?key=mzk_G8D0BIPFX70FXUYEUL5&link=${encodeURIComponent(linkanh)}`);    
+var img = res.data.NSFW_Prob;
+    return api.sendMessage(`tỷ lệ nude của ảnh là: ${img}`, event.threadID, event.messageID);
+	
+} else if (args[0] == "all") {
+      let threadInfo = await api.getThreadInfo(event.threadID);
+        let number = 0, msg = "", storage = [], exp = [];
+        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+        for (const user of storage) {
+            const countMess = await Currencies.getData(user.id);
+            if (user.name != null) exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp});
+        }
+        exp.sort((a, b) => {
+            if (a.exp > b.exp) return -1;
+            if (a.exp < b.exp) return 1;
+        });
+         let rank = exp.findIndex(info => parseInt(info.uid) == parseInt(`${(event.type == "message_reply") ? event.messageReply.senderID : event.senderID}`)) + 1;
+        let infoUser = exp[rank - 1];
+        for (const lastData of exp) {
+            number++;
+            msg += `『${number}』: ${(lastData.name) == null || undefined  ? "Không tên" : lastData.name} với ${lastData.exp} tin nhắn \n`;
+        }
+        return api.sendMessage(`==「KIỂM TRA TƯƠNG TÁC」==\n\n` + msg +`\n» 💹Tỉ lệ tương tác: ${(exp[rank].exp).toFixed(0)}%\n» 💬 Tổng số tin nhắn: ${threadInfo.messageCount}\n    === 「${timeNow}」 ===`, threadID, messageID);
+    }
+    
+    else if (args[0] == "tuongtac") {
+        let storage = [], exp = [];
+        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+        for (const user of storage) {
+            const countMess = await Currencies.getData(user.id);
+            exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+        }
+        exp.sort((a, b) => {
+            if (a.exp > b.exp) return -1;
+            if (a.exp < b.exp) return 1;
+            if (a.id > b.id) return 1;
+		    if (a.id < b.id) return -1;
+        });
+        let rank = exp.findIndex(info => parseInt(info.uid) == parseInt(`${(event.type == "message_reply") ? event.messageReply.senderID : event.senderID}`)) + 1;
+        let infoUser = exp[rank - 1];
+        return api.sendMessage(`\n» Bạn`+` đứng hạng ${rank} với ${infoUser.exp} tin nhắn\n» 👤Tên: ${infoUser.name}\n» ⬛Hạng: ${rank} \n» 💬Tin nhắn: ${infoUser.exp}\n» 🏆Rank: ${rank + 1}\n» 💹Tỉ lệ tương tác: ${(exp[rank].exp).toFixed(0)}%\n» ⏰Time: ${timeNow}`, event.threadID,event.messageID);
+    }
+  else if (args[0] == "()") {
+        let storage = [], exp = [];
+        for (const value of data.userInfo) storage.push({"id" : value.id, "name": value.name});
+        for (const user of storage) {
+            const countMess = await Currencies.getData(user.id);
+            exp.push({"name" : user.name, "exp": (typeof countMess.exp == "undefined") ? 0 : countMess.exp, "uid": user.id});
+        }
+        exp.sort((a, b) => {
+            if (a.exp > b.exp) return -1;
+            if (a.exp < b.exp) return 1;
+            if (a.id > b.id) return 1;
+		    if (a.id < b.id) return -1;
+        });
+        let rank = exp.findIndex(info => parseInt(info.uid) == parseInt(`${(event.type == "message_reply") ? event.messageReply.senderID : event.senderID}`)) + 1;
+        let infoUser = exp[rank - 1];
+        return api.sendMessage(`\n» Bạn`+` đứng hạng ${rank} với ${infoUser.exp} tin nhắn\n» 👤Tên: ${infoUser.name}\n» ⬛Hạng: ${rank} \n» 💬Tin nhắn: ${infoUser.exp}\n» 🏆Rank: ${rank + 1}\n» 💹Tỉ lệ tương tác: ${(exp[rank].exp).toFixed(0)}%\n» ⏰Time: ${timeNow}`, event.threadID,event.messageID);
+    }
+}
