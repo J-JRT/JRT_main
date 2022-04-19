07c52873067782b37dfbd43dcc6095a327a1e004
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-04-19 22:02:26 +0700

    Delete handleUnsend.js

--- includes/handle/handleUnsend.js
+++
@@ -1,9 +0,0 @@
-module.exports = function({ api }) {
- const reaction = "❤";
-	return function({ event }) {
- const { senderID, reaction, messageID } = event;
- if (senderID == api.getCurrentUserID()) {
- if (reaction == "❤") return api.unsendMessage(messageID);
- }
-	};
-};
