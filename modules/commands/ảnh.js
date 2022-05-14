4752a253a49b98f3f9ab2c55d4805f2784355ea0
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-05-14 21:53:05 +0700

    Update ảnh.js

--- modules/commands/ảnh.js
+++ modules/commands/ảnh.js
@@ -17,7 +17,7 @@ module.exports.config = {
   module.exports.run = async function ({ event, api , args, Users}){
     const fs = require("fs");
     let name1 = await Users.getNameUser(event.senderID)
-    var name = ["Sexy", "Gái nga", "Ganyu", "Twitter", "LGBT", "Loli", "Blackpink", "Tát", "Wallpaper", "Trai", "Jack", "Nude", "Instagram", "Kiss", "Ngực", "Meme", "Hentai", "Gái", "Mông", "Cosplay", "Kurumi", "Liên quân", "Lucy", "Sagiri", "Chitanda", "Rem", "Anime", "Naughty", "Wibu", "Beo", "Ausand", "Shiba", "Khánh Huyền", "Ngọc Trinh", "Linh Ngọc Đàm", "Jimmy", "Lê Bống", "Sex", "Độ Mixi", "Cặp đôi", "Streamer Hanna", "Nobra", "Gái Sexy", "Gái Xinh", "Trai đẹp", "Background 4K", "Anime Hot", "Gái Japan", "Gái China", "Hololive", "Hot Twitter", "Hot Instagram", "Gái VSBG", "Ảnh Phan Trần Anh Tâm", "Ảnh Sex 18+", "Japan", "VSBG Hot"]
+    var name = ["Sexy", "Gái nga", "Ganyu", "Twitter", "LGBT", "Loli", "Blackpink", "Tát", "Wallpaper", "Trai", "Jack", "Nude", "Instagram", "Kiss", "Ngực", "Meme", "Hentai", "Gái", "Mông", "Cosplay", "Kurumi", "Liên quân", "Lucy", "Sagiri", "Chitanda", "Rem", "Anime", "Naughty", "Wibu", "Beo", "Ausand", "Shiba", "Khánh Huyền", "Ngọc Trinh", "Linh Ngọc Đàm", "Jimmy", "Lê Bống", "Sex", "Độ Mixi", "Cặp đôi", "Streamer Hanna", "Nobra", "Gái Sexy", "Gái Xinh", "Trai đẹp", "Background 4K", "Anime Hot", "Gái Japan", "Gái China", "Hololive", "Hot Twitter", "Hot Instagram", "Gái VSBG", "Ảnh Phan Trần Anh Tâm", "Ảnh Sex 18+", "Japan", "VSBG Hot", "Gái Xinh Việt Nam"]
     var b = name.length;
     var page = 1;
     page = parseInt(args[0]) || 1;
@@ -46,88 +46,88 @@ module.exports.config = {
     const axios = require("axios");
     
              if(event.body == "1"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/gaisexy"
+         var url = "https://jrt-api.jrt-official.repl.co/gaisexy"
 }
          else if(event.body == "2"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/gainga"
+         var url = "https://jrt-api.jrt-official.repl.co/gainga"
 }
          else if(event.body == "3"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/ganyu"
+         var url = "https://jrt-api.jrt-official.repl.co/ganyu"
 }
           else if(event.body == "4"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/twitter"
+          var url = "https://jrt-api.jrt-official.repl.co/twitter"
 }
           else if(event.body == "5"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/lgbt"
+          var url = "https://jrt-api.jrt-official.repl.co/lgbt"
 }
          else if(event.body == "6"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/loli"
+          var url = "https://jrt-api.jrt-official.repl.co/loli"
 }
          else if(event.body == "7"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/blackpink"
+          var url = "https://jrt-api.jrt-official.repl.co/blackpink"
 }
          else if(event.body == "8"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/slap"
+          var url = "https://jrt-api.jrt-official.repl.co/slap"
 }
          else if(event.body == "9"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/wallpaper"
+         var url = "https://jrt-api.jrt-official.repl.co/wallpaper"
 }
          else if(event.body == "10"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/trai"
+         var url = "https://jrt-api.jrt-official.repl.co/trai"
 }
          else if(event.body == "11"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/jack"
+         var url = "https://jrt-api.jrt-official.repl.co/jack"
 }
         else if(event.body == "12"){
-          var  url = "https://jrt-api.j-jrt-official.repl.co/nude"
+          var  url = "https://jrt-api.jrt-official.repl.co/nude"
 }
          else if(event.body == "13"){
-          var  url = "https://jrt-api.j-jrt-official.repl.co/instagram"
+          var  url = "https://jrt-api.jrt-official.repl.co/instagram"
 }
          else if(event.body == "14"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/kiss"
+         var url = "https://jrt-api.jrt-official.repl.co/kiss"
 }
          else if(event.body == "15"){
-         var url = "https://jrt-api.j-jrt-official.repl.co/gaivuto"
+         var url = "https://jrt-api.jrt-official.repl.co/gaivuto"
 }
          else if(event.body == "16"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/meme"
+          var url = "https://jrt-api.jrt-official.repl.co/meme"
 }
          else if(event.body == "17"){
-           var url = "https://jrt-api.j-jrt-official.repl.co/hentai"
+           var url = "https://jrt-api.jrt-official.repl.co/hentai"
 }
          else if(event.body == "18"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/gai"
+          var url = "https://jrt-api.jrt-official.repl.co/gai"
 }
          else if(event.body == "19"){
-           var url = "https://jrt-api.j-jrt-official.repl.co/gaiditbu"
+           var url = "https://jrt-api.jrt-official.repl.co/gaiditbu"
 }
          else if(event.body == "20"){
-          var url = "https://jrt-api.j-jrt-official.repl.co/cosplay"
+          var url = "https://jrt-api.jrt-official.repl.co/cosplay"
 }
         else if(event.body == "21"){
-          var url = "https://api-jrt.j-jrt-official.repl.co/kurumi.php"
+          var url = "https://ApiMyJRT.jrt-official.repl.co/kurumi.php"
 }
        else if(event.body == "22"){
-          var url = "https://api-jrt.j-jrt-official.repl.co/lienquan.php"
+          var url = "https://ApiMyJRT.jrt-official.repl.co/lienquan.php"
 }
        else if(event.body == "23"){
-          var url = "https://api-jrt.j-jrt-official.repl.co/lucy.php"
+          var url = "https://ApiMyJRT.jrt-official.repl.co/lucy.php"
 }
        else if(event.body == "24"){
-          var url = "https://api-jrt.j-jrt-official.repl.co/sagiri.php"
+          var url = "https://ApiMyJRT.jrt-official.repl.co/sagiri.php"
 }
        else if(event.body == "25"){
-          var url = "https://api-jrt.j-jrt-official.repl.co/chitanda.php"
+          var url = "https://ApiMyJRT.jrt-official.repl.co/chitanda.php"
 }
        else if(event.body == "26"){
-           var url = "https://api-jrt.j-jrt-official.repl.co/rem.php"
+           var url = "https://ApiMyJRT.jrt-official.repl.co/rem.php"
 }
        else if(event.body == "27"){
-         var  url = "https://api-jrt.j-jrt-official.repl.co/loli.php"
+         var  url = "https://ApiMyJRT.jrt-official.repl.co/loli.php"
 }
        else if(event.body == "28"){
-           var url = "https://api-jrt.j-jrt-official.repl.co/naughty.php "
+           var url = "https://ApiMyJRT.jrt-official.repl.co/naughty.php "
 }
        else if(event.body == "29"){
            var url = "https://wibu.demngayyeu.repl.co/"
@@ -157,64 +157,67 @@ module.exports.config = {
         var url = "https://lebong.demngayyeu.repl.co/"
 }
        else if(event.body == "38"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/nude.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/nude.php"
 }
         else if(event.body == "39"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/domixi"
+        var url = "https://jrt-api.jrt-official.repl.co/domixi"
 }
         else if(event.body == "40"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/capdoi"
+        var url = "https://jrt-api.jrt-official.repl.co/capdoi"
 }
         else if(event.body == "41"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/hanna"
+        var url = "https://jrt-api.jrt-official.repl.co/hanna"
 }
         else if(event.body == "42"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/nobra.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/nobra.php"
 }
         else if(event.body == "43"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/sexy.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/sexy.php"
 }
         else if(event.body == "44"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/gái.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/gái.php"
 }
         else if(event.body == "45"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/trai.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/trai.php"
 }
         else if(event.body == "46"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/background.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/background.php"
 }
         else if(event.body == "47"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/anime"
+        var url = "https://jrt-api.jrt-official.repl.co/anime"
 }
         else if(event.body == "48"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/japan.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/japan.php"
 }
         else if(event.body == "49"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/china.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/china.php"
 }
     else if(event.body == "50"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/hololive.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/hololive.php"
 }
     else if(event.body == "51"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/twitter.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/twitter.php"
 }
     else if(event.body == "52"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/instagram.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/instagram.php"
 }
     else if(event.body == "53"){
-        var url = "https://api-jrt.j-jrt-official.repl.co/vsbg.php"
+        var url = "https://ApiMyJRT.jrt-official.repl.co/vsbg.php"
 }
     else if(event.body == "54"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/ptat"
+        var url = "https://jrt-api.jrt-official.repl.co/ptat"
 }
     else if(event.body == "55"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/sex"
+        var url = "https://jrt-api.jrt-official.repl.co/sex"
 }
     else if(event.body == "56"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/japan"
+        var url = "https://jrt-api.jrt-official.repl.co/japan"
 }
     else if(event.body == "57"){
-        var url = "https://jrt-api.j-jrt-official.repl.co/vsbg"
+        var url = "https://jrt-api.jrt-official.repl.co/vsbg"
+}
+    else if(event.body == "58"){
+        var url = "https://jrt-api.jrt-official.repl.co/gaixinhvn"
 }
     switch(handleReply.type){
     case "choose":{
