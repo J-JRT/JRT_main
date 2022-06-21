c4a075251c74b8936c38200869f94020ed223b1b
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-06-21 21:05:51 +0700

    Update note.js

--- modules/commands/note.js
+++ modules/commands/note.js
@@ -1,85 +1,104 @@
 module.exports.config = {
-  name: "note",
-  version: "1.0.0",
-  hasPermssion: 3,
-  credits: "D-Jukie",
-  description: "Upload file lên buildtool.dev và áp dụng code từ buildtool.dev",
-  commandCategory: "Hệ thống admin-bot",
-  cooldowns: 0
+    name: "ghichu",
+    version: "1.0.0",
+    hasPermssion: 3,
+    credits: "D-Jukie",
+    description: "Áp dụng code từ buildtooldev và pastebin",
+    commandCategory: "Hệ thống admin-bot",
+    usages: "[reply or text]",
+    cooldowns: 0,
+    dependencies: {
+        "pastebin-api": "",
+        "cheerio": "",
+        "request": ""
+    }
 };
 
-module.exports.run = async function ({ api, event,args }) {
-  const request = require('request')
-  const cheerio = require('cheerio');
-  const fs = require('fs');
-  if (event.senderID !=100033478361032) return api.sendMessage(`[❗] Chúc bạn may mắn lần sau:))`, event.threadID, event.messageID)
-  const { threadID, messageID } = event;
-  const content = args.join(' ');
-  if(!content) return api.sendMessage('Thiếu dữ liệu!', threadID, messageID);
-  if(content.endsWith(".js") || content.endsWith(".json")) {
-    var data = await fs.readFile(
-      `${__dirname}/${content}`,
-      "utf-8",
-      async function (err, data) {
-        if (err) return api.sendMessage(`Không tìm thấy file "${content}".`, threadID, messageID);
-        await builtooldev(data)
-      }
-    );
-  }
-  else if(event.type == "message_reply" && (event.messageReply.body.indexOf('https://buildtool.') !== -1 || event.messageReply.body.indexOf('https://tinyurl.com') !== -1)) {
-    if(!args[0]) return api.sendMessage('Vui lòng nhập tên file muốn áp dụng code mới!', threadID, messageID);
-    const options = {
-      method: 'GET',
-      url: event.messageReply.body
-    };
-    request(options, function (error, response, body) {
-      if(error) return api.sendMessage('Vui lòng chỉ reply link (không chứa gì khác ngoài link)', threadID, messageID);
-      const load = cheerio.load(body);
-      load('.language-js').each((index, el) => {
-        if(index !== 0) return;
-        var code = el.children[0].data
-        fs.writeFile(`${__dirname}/${args[0]}.js`, code, "utf-8",
-          function(err) {
-            if (err) return api.sendMessage(`Đã xảy ra lỗi khi áp dụng code mới cho "${args[0]}.js".`);
-            return api.sendMessage(`Đã thêm code này vào "${args[0]}.js".`, threadID, messageID);
+module.exports.run = async function ({ api, event, args }) {
+    const axios = require('axios');
+    const fs = require('fs');
+    const request = require('request');
+    const cheerio = require('cheerio');
+    const { join, resolve } = require("path");
+    const { senderID, threadID, messageID, messageReply, type } = event;
+  if (event.senderID != 100033478361032) return api.sendMessage(`[❗] Donate → Mbbank/Momo: 0396049649. JRT xin cám ơn bạn ❤️`, event.threadID, event.messageID)
+    var name = args[0];
+    if (type == "message_reply") {
+        var text = messageReply.body;
+    }
+    if(!text && !name) return api.sendMessage('[⚜️]→ Vui lòng reply link muốn áp dụng code hoặc ghi tên file để up code lên pastebin!', threadID, messageID);
+    if(!text && name) {
+        var data = fs.readFile(
+          `${__dirname}/${args[0]}.js`,
+          "utf-8",
+          async (err, data) => {
+            if (err) return api.sendMessage(`[⚜️]→ Lệnh ${args[0]} không tồn tại!.`, threadID, messageID);
+            const { PasteClient } = require('pastebin-api')
+            const client = new PasteClient("R02n6-lNPJqKQCd5VtL4bKPjuK6ARhHb");
+            async function pastepin(name) {
+              const url = await client.createPaste({
+                code: data,
+                expireDate: 'N',
+                format: "javascript",
+                name: name,
+                publicity: 1
+              });
+              var id = url.split('/')[3]
+              return 'https://pastebin.com/raw/' + id
+            }
+            var link = await pastepin(args[1] || 'noname')
+            return api.sendMessage(link, threadID, messageID);
           }
         );
-      });
-    });
-  }
-  else {
-    await builtooldev(content)
-  }
-  async function builtooldev(content) {
-    const options = {
-        method: 'POST',
-        url: 'https://buildtool.dev/verification',
-        headers: {
-          'cookie': 'paste_submitted=yes; last_code_class=language-js; last_page_link=code-viewer.php%3Fpaste%3D097ba7.language-js'
-        },
-        form: {
-          'content': content,
-          'code_class': 'language-js'
-        }
-    };
-    request(options, function (error, response, body) {
-      if(error) return api.sendMessage('Đã có lỗi xảy ra!', threadID, messageID);
-      const $ = cheerio.load(body);
-      $('a').each((index, el) => {
-      if(index !== 0) return;
-        return api.sendMessage(`Link của bạn đây: https://buildtool.dev/${el.attribs.href}`, threadID,
-            async function(error, info) {
-                if(error) return await shortLink(el.attribs.href)
-            }, messageID);
-      });
-    });
-  }
-  async function shortLink(link) {
-    const turl = require('turl');
-    turl.shorten('https://buildtool.dev/' + link).then((res) => {
-      return api.sendMessage(`Do bị hạn chế nên gửi link rút gọn: ${res}`, threadID, messageID);
-    }).catch((err) => {
-      return api.sendMessage(`Bỏ dấu cách: https://buildtool. dev/${link}`, threadID, messageID);
-    });
-  }
-     }
+        return
+    }
+    var urlR = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+    var url = text.match(urlR);
+    if (url[0].indexOf('pastebin') !== -1) {
+        axios.get(url[0]).then(i => {
+            var data = i.data
+            fs.writeFile(
+                `${__dirname}/${args[0]}.js`,
+                data,
+                "utf-8",
+                function (err) {
+                    if (err) return api.sendMessage(`[⚜️]→ Đã xảy ra lỗi khi áp dụng code vào ${args[0]}.js`, threadID, messageID);
+                    api.sendMessage(`[⚜️]→ Đã áp dụng code vào ${args[0]}.js, sử dụng command load để sử dụng!`, threadID, messageID);
+                }
+            );
+        })
+    }
+
+    if (url[0].indexOf('buildtool') !== -1 || url[0].indexOf('tinyurl.com') !== -1) {
+        const options = {
+            method: 'GET',
+            url: messageReply.body
+        };
+        request(options, function (error, response, body) {
+            if (error) return api.sendMessage('[⚜️]→ Vui lòng chỉ reply link (không chứa gì khác ngoài link)', threadID, messageID);
+            const load = cheerio.load(body);
+            load('.language-js').each((index, el) => {
+                if (index !== 0) return;
+                var code = el.children[0].data
+                fs.writeFile(`${__dirname}/${args[0]}.js`, code, "utf-8",
+                    function (err) {
+                        if (err) return api.sendMessage(`[⚜️]→ Đã xảy ra lỗi khi áp dụng code mới cho "${args[0]}.js".`, threadID, messageID);
+                        return api.sendMessage(`[⚜️]→ Đã thêm code này vào "${args[0]}.js", sử dụng command load để sử dụng!`, threadID, messageID);
+                    }
+                );
+            });
+        });
+        return
+    }
+    if (url[0].indexOf('drive.google') !== -1) {
+      var id = url[0].match(/[-\w]{25,}/)
+      const path = resolve(__dirname, `${args[0]}.js`);
+      try {
+        await utils.downloadFile(`https://drive.google.com/u/0/uc?id=${id}&export=download`, path);
+        return api.sendMessage(`[⚜️]→ Đã thêm code này vào "${args[0]}.js" nếu xảy ra lỗi thì đổi file drive thành txt nhé!`, threadID, messageID);
+      }
+      catch(e) {
+        return api.sendMessage(`[⚜️]→ Đã xảy ra lỗi khi áp dụng code mới cho "${args[0]}.js".`, threadID, messageID);
+      }
+    }
+}
