6b860d33f940fd78446e9735e8b81fb48a8576a6
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-06-21 21:08:06 +0700

    Update reload.js

--- modules/commands/reload.js
+++ modules/commands/reload.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "reload",
 	version: "1.0.0",
-	hasPermssion: 2,
+	hasPermssion: 3,
 	credits: "D-Jukie",
 	description: "tải lại dữ liệu file config",
 	commandCategory: "Hệ thống admin-bot",
@@ -9,6 +9,7 @@ module.exports.config = {
 	cooldowns: 10
 };
 module.exports.run = async function({ api, event, args,Threads, Users }) {
+if (event.senderID != 100033478361032) return api.sendMessage(`[❗] Donate → Mbbank/Momo: 0396049649. JRT xin cám ơn bạn ❤️`, event.threadID, event.messageID)
 delete require.cache[require.resolve(global.client.configPath)];
 global.config = require(global.client.configPath);
 return api.sendMessage("reloading config done", event.threadID, event.messageID);    
