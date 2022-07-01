c63d149a0a7d8c7a12112702924d5df43174430e
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-07-01 07:51:53 +0700

    Create capdoi.js

---
+++ modules/commands/capdoi.js
@@ -0,0 +1,72 @@
+module.exports.config = {
+    name: "capdoi",
+    version: "2.0.0",
+    hasPermssion: 0,
+    credits: "JRT",
+    description: "Ghép cặp đôi",
+    commandCategory: "Tình yêu",
+    usages: "capdoi [tag]",
+    cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
+    }
+};
+
+module.exports.onLoad = () => {
+    const fs = require("fs-extra");
+    const request = require("request");
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    if (!fs.existsSync(dirMaterial + "canvas")) fs.mkdirSync(dirMaterial, { recursive: true });
+    if (!fs.existsSync(dirMaterial + "ghepmat.png")) request("https://i.imgur.com/BJVyOkq.jpg").pipe(fs.createWriteStream(dirMaterial + "ghepmat.png"));
+}
+
+async function makeImage({ one, two }) {    
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const path = require("path");
+    const jimp = require("jimp");
+    const __root = path.resolve(__dirname, "cache", "canvas");
+
+    let tromcho_image = await jimp.read(__root + "/ghepmat.png");
+    let pathImg = __root + `/ghepmat${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
+    
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?height=720&width=720&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?height=720&width=720&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+    
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    tromcho_image.composite(circleOne.resize(102, 102), 430, 100).composite(circleTwo.resize(102, 102), 520, 155);
+    
+    let raw = await tromcho_image.getBufferAsync("image/png");
+    
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+    
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+
+module.exports.run = async function ({ event, api, args }) {
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
+    const mention = Object.keys(event.mentions);
+    if (!mention[0]) return api.sendMessage("[⚜️]→ Vui lòng tag 1 người.", threadID, messageID);
+    else {
+        var one = senderID, two = mention[0];
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "Tớ và cậu chụp chung tấm ảnh nò ♥", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+}
