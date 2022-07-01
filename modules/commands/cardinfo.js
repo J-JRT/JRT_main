babb3fc647553f24113946210d8dbb1fb0778344
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-07-01 07:35:39 +0700

    Update cardinfo.js

--- modules/commands/cardinfo.js
+++ modules/commands/cardinfo.js
@@ -16,8 +16,8 @@ module.exports.config = {
   usages: "cardinfo",
   cooldowns: 10,
   dependencies: {
-    canvas: "",
-    axios: "",
+    "canvas": "",
+    "axios": "",
     "fs-extra": "",
   },
 };
@@ -42,7 +42,7 @@ module.exports.run = async function ({ api, event, args, Users }) {
   if(event.type == "message_reply") { uid = event.messageReply.senderID }
     else uid = event.senderID;
     const res = await api.getUserInfoV2(uid); 
-  let getAvatarOne = (await axios.get(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=1073911769817594|aa417da57f9e260d1ac1ec4530b417de`, { responseType: 'arraybuffer' })).data;
+  let getAvatarOne = (await axios.get(`https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
   let bg = (
     await axios.get(encodeURI(`https://i.imgur.com/8UaB48J.png`), {
       responseType: "arraybuffer",
